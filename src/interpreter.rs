8bca1ecc7cca2bc3c95a3a3f47dfe424b7143534
Sebastian Krzyszkowiak <dos@dosowisko.net>
2016-04-29 03:14:48 +0200

    move State struct to separate module

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,64 +1,8 @@
+use state::State;
 use std::char;
 use std::io;
 use std::io::Read;
-use std::num::Wrapping;
-use std::fmt;
 use std::ascii::AsciiExt;
-
-const MEMORY_SIZE: usize = 100000;
-
-struct State {
-    pointer: i32,
-    memory: [Wrapping<i32>; MEMORY_SIZE]
-}
-
-impl fmt::Display for State {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "<pointer address: {}, value: {}>", self.get_pointer(), self.get_value())
-    }
-}
-
-impl State {
-    pub fn new() -> State { 
-        State { pointer: 0, memory: [Wrapping(0); MEMORY_SIZE] }
-    }
-    
-    fn add_to_pointer(&mut self, value: i32) {
-        self.pointer = self.pointer + value;
-    }
-    
-    pub fn increment_pointer(&mut self) {
-        self.add_to_pointer(1)
-    }
-    
-    pub fn decrement_pointer(&mut self) {
-        self.add_to_pointer(-1)
-    }
-    
-    pub fn set_value(&mut self, value: i32) {
-        self.memory[self.pointer as usize] = Wrapping(value);
-    }
-    
-    pub fn get_value(&self) -> i32 {
-        self.memory[self.pointer as usize].0
-    }
-    
-    fn get_pointer(&self) -> i32 {
-        self.pointer
-    }
-    
-    fn add_to_value(&mut self, value: i32) {
-        self.memory[self.pointer as usize] += Wrapping(value);
-    }
-    
-    pub fn increment_value(&mut self) {
-        self.add_to_value(1)
-    }
-    
-    pub fn decrement_value(&mut self) {
-        self.add_to_value(-1)
-    }
-}
 
 pub fn run(code: &str) {
     let mut state = State::new();
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod input;
 mod interpreter;
+mod state;
+use input::Input;
 use std::io::Read;
 use std::env;
 
 fn main() {
-    let mut stream = input::Input::from_arg(env::args().nth(1)).unwrap();
+    let mut stream = Input::from_arg(env::args().nth(1)).unwrap();
     let mut buffer = String::new();
     stream.read_to_string(&mut buffer).unwrap();
     interpreter::run(&*buffer);
